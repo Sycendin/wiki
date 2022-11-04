8b707052d831146396843ea7c8e5c55964d6c8e8
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-11-04 00:40:31 -0400

    update: add static paths to articles

--- pages/articles/[id]/index.js
+++ pages/articles/[id]/index.js
@@ -12,4 +12,31 @@ const article = ({ article }) => {
     </div>
   );
 };
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
+
+  const article = await res.json();
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`${server}/api/articles/`);
+
+  const articles = await res.json();
+  const ids = articles.map((article) => article.id);
+  // Generate paths for ids
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+  return {
+    // return paths
+    paths,
+    // go to 404 page if doesnt exist
+    fallback: false,
+  };
+};
+
 export default article;
