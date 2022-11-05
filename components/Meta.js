45b545de49d191d39f0a8953a09ebf1067d60fbb
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-11-05 00:30:44 -0400

    update: add meta component:

---
+++ components/Meta.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, intial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>{title}</title>
+    </Head>
+  );
+};
+Meta.defaultProps = {
+  title: "wiki shows",
+  keywords: "wiki shows",
+  description: "latest wiki news",
+};
+export default Meta;
