7f31f364bde933585bb8992c1bff4ea9daccebe5
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-11-23 23:01:36 -0500

    update: change data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+export const articles = [
+  {
+    id: "1",
+    title: "Better Call Saul",
+    excerpt: "why is that",
+    body: "send help",
+  },
+  {
+    id: "2",
+    title: "Chicago PD",
+    excerpt: "why is that",
+    body: "send help",
+  },
+  {
+    id: "3",
+    title: "Breaking Bad",
+    excerpt: "why is that",
+    body: "send help",
+  },
+  {
+    id: "4",
+    title: "Pokemon",
+    excerpt: "why is that",
+    body: "send help",
+  },
+];
