73d09e2246456b03ee6bc3abba1a3ee7077d4c6e
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-01-06 23:32:15 -0500

    update: folder restructure and add dropdown to search

---
+++ components/CharPage/SearchPlusList.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import { React, useState } from "react";
+import dynamic from "next/dynamic";
+
+const DynamicHeader = dynamic(() => import("./Characters/Dropdown/Dropdown"), {
+  ssr: false,
+});
+import ArticleList from "./Characters/ArticleList";
+import Search from "./Characters/Search";
+// import Dropdown from "./Characters/Dropdown/Dropdown";
+const SearchPlusList = ({ articles }) => {
+  const [inputText, setInputText] = useState("");
+  return (
+    <Fragment>
+      <div className="container is-centered  mb-3 border ">
+        <DynamicHeader />
+      </div>
+      <Search setInputText={setInputText} />
+      <ArticleList articles={articles} inputText={inputText} />
+    </Fragment>
+  );
+};
+export default SearchPlusList;
