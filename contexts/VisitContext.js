062f5fee60b50c3b583d40f65a5581b3dd9d2c2b
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-02-05 22:30:28 -0500

    add effect to popup div

--- contexts/VisitContext.js
+++ contexts/VisitContext.js
@@ -3,15 +3,20 @@ import { createContext, useState } from "react";
 export const VisitContext = createContext({
   visit: false,
   setVisit: () => null,
+  timeCheck: false,
+  setTimeCheck: () => null,
 });
 
 export const VisitProvider = ({ children }) => {
   const [visit, setVisit] = useState("True");
-
+  const [timeCheck, setTimeCheck] = useState(false);
   const changeVisit = (value) => {
     setVisit(value);
   };
-  const value = { visit, changeVisit };
+  const changeTimeCheck = (value) => {
+    setTimeCheck(value);
+  };
+  const value = { visit, changeVisit, timeCheck, changeTimeCheck };
   return (
     <VisitContext.Provider value={value}>{children}</VisitContext.Provider>
   );
