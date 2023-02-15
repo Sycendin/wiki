e73a2933c14ef7b3ea2e64439ba9b2f1318e596d
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-02-14 22:22:33 -0500

    update: add lastupdated

--- components/CharPage/LastUpdated/LastUpdated.js
+++ components/CharPage/LastUpdated/LastUpdated.js
@@ -6,7 +6,9 @@ const LastUpdated = () => {
     <Fragment>
       <div className={`${styles.updateDiv}`}>
         <span className={`${styles.updateSpan}`}>
-          <p className={`${styles.updateText}`}></p>
+          <p className={`${styles.updateText}`}>
+            Characters last updated February 11th, 2023
+          </p>
         </span>
       </div>
     </Fragment>
