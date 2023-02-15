5867bd441c45dac86713875b1be20310c26d07eb
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-02-15 01:09:47 -0500

    update: date now stored in chardata

--- components/CharPage/LastUpdated/LastUpdated.js
+++ components/CharPage/LastUpdated/LastUpdated.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-
+import { lastUpdatedData } from "../../../chardata";
 import styles from "../../../styles/lastupdated.module.css";
 const LastUpdated = () => {
   return (
@@ -7,7 +7,7 @@ const LastUpdated = () => {
       <div className={`${styles.updateDiv}`}>
         <span className={`${styles.updateSpan}`}>
           <p className={`${styles.updateText}`}>
-            Characters last updated February 11th, 2023
+            Characters last updated {lastUpdatedData}
           </p>
         </span>
       </div>
