46b18b634c14ff91339e376cc9ae8aedc9c065de
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-02-21 01:12:56 -0500

    update: add more info to combatdiv

--- components/IndivChar/CombatInfo/CombatInfo.js
+++ components/IndivChar/CombatInfo/CombatInfo.js
@@ -12,6 +12,26 @@ const CombatInfo = ({ data }) => {
           <p className={`${styles.combatLeftText} white`}>Rarity</p>
           <p className={`${styles.combatRightText} white`}>{rarity}</p>
         </div>
+        <div className={`${styles.combatRowDiv}`}>
+          {" "}
+          <p className={`${styles.combatLeftText} white`}>Class</p>
+          <p className={`${styles.combatRightText} white`}>{unitClass}</p>
+        </div>
+        <div className={`${styles.combatRowDiv}`}>
+          {" "}
+          <p className={`${styles.combatLeftText} white`}>Weapon</p>
+          <p className={`${styles.combatRightText} white`}>{weapon}</p>
+        </div>
+        <div className={`${styles.combatRowDiv}`}>
+          {" "}
+          <p className={`${styles.combatLeftText} white`}>element</p>
+          <p className={`${styles.combatRightText} white`}>{element}</p>
+        </div>
+        <div className={`${styles.combatRowDiv}`}>
+          {" "}
+          <p className={`${styles.combatLeftText} white`}>Burst Type</p>
+          <p className={`${styles.combatRightText} white`}>{burst}</p>
+        </div>
       </div>
     </Fragment>
   );
