4fce61c1e51fe73e642194f74a212bdfc847e127
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-02-22 22:44:10 -0500

    update: add more data for indiv char page

--- components/IndivChar/IndivChar.js
+++ components/IndivChar/IndivChar.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import CombatInfo from "./CombatInfo/CombatInfo";
-
+import CharInfo from "./CharInfo/CharInfo";
+import styles from "../../styles/indivchar.module.css";
 const IndivChar = ({ data }) => {
   return (
     <Fragment>
-      <div>
+      <div className={`${styles.indivCharDiv}`}>
+        <CharInfo data={data} />
         <CombatInfo data={data} />
       </div>
     </Fragment>
