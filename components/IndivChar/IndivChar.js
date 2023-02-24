4464adc4ba7f51bd70665519c174fed09fd14821
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-02-24 00:07:49 -0500

    update: update css and add more info for indiv char

--- components/IndivChar/IndivChar.js
+++ components/IndivChar/IndivChar.js
@@ -1,13 +1,18 @@
 import { Fragment } from "react";
 import CombatInfo from "./CombatInfo/CombatInfo";
 import CharInfo from "./CharInfo/CharInfo";
+import OtherInfo from "./OtherInfo/OtherInfo";
 import styles from "../../styles/indivchar.module.css";
 const IndivChar = ({ data }) => {
   return (
     <Fragment>
-      <div className={`${styles.indivCharDiv}`}>
-        <CharInfo data={data} />
-        <CombatInfo data={data} />
+      <div className={`${styles.indivCharHeader} white`}>
+        <p className={`${styles.indivCharHeaderText} white`}>Info</p>
+        <div className={`${styles.indivCharDiv}`}>
+          <CharInfo data={data} />
+          <CombatInfo data={data} />
+          <OtherInfo data={data} />
+        </div>
       </div>
     </Fragment>
   );
