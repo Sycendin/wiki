d1961f4b4d6bd8966cd59969f983fa3b110654d0
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-03-03 04:09:28 -0500

    update: update style for profile section of ichar

--- components/IndivChar/CharProfile/CharProfile.js
+++ components/IndivChar/CharProfile/CharProfile.js
@@ -2,9 +2,15 @@ import { Fragment } from "react";
 
 import styles from "../../../styles/charprofile.module.css";
 const CharProfile = ({ data }) => {
+  const [name, art] = data;
   return (
     <Fragment>
-      <div className={`${styles.charProfile}`}></div>
+      <div className={`${styles.charProfile}`}>
+        <img className={`${styles.charImage}`} src={art}></img>
+        <div className={`${styles.charInfoDiv}`}>
+          <p className={`${styles.charName}`}>{name}</p>
+        </div>
+      </div>
     </Fragment>
   );
 };
