c9d12b2da889f0c1acb4e38afaeb32ff5341cb29
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-03-17 02:18:53 -0400

    updaet: add imports for skillicons

--- components/IndivChar/Skills/Skills.js
+++ components/IndivChar/Skills/Skills.js
@@ -3,15 +3,16 @@ import SkillAttack from "./SkillAttack/SkillAttack";
 import SkillOne from "./SkillOne/SkillOne";
 import SkillTwo from "./SkillTwo/SkillTwo";
 import SkillBurst from "./SkillBurst/SkillBurst";
+import { skillIcons } from "../../../chardata";
 import styles from "../../../styles/skill.module.css";
 const Skills = ({ data }) => {
   return (
     <Fragment>
       <div className={`${styles.skillDiv}`}>
-        <SkillAttack data={data} />
-        <SkillOne data={data} />
-        <SkillTwo data={data} />
-        <SkillBurst data={data} />
+        <SkillAttack data={data} skillIcons={skillIcons} />
+        <SkillOne data={data} skillIcons={skillIcons} />
+        <SkillTwo data={data} skillIcons={skillIcons} />
+        <SkillBurst data={data} skillIcons={skillIcons} />
       </div>
     </Fragment>
   );
