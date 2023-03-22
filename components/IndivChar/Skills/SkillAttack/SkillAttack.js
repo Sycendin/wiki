275012c683e1f2e02a95af8eb7e6deb4362e8a64
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-03-21 22:56:06 -0400

    update: update skill css file

--- components/IndivChar/Skills/SkillAttack/SkillAttack.js
+++ components/IndivChar/Skills/SkillAttack/SkillAttack.js
@@ -7,28 +7,33 @@ const SkillAttack = ({ data, skillIcons }) => {
       {/* <div className={`${styles.skillDiv}`}>
         <img src={skillIcons.normal}></img>
       </div> */}
-      <div className={`${styles.charProfile}`}>
-        <img className={`${styles.charImage}`} src={skillIcons.normal}></img>
-        <div className={`${styles.charInfoDiv}`}>
-          <p className={`${styles.charName}`}>Normal Attack</p>
-          <div className={`${styles.charInfoDivInner}`}>
-            <div className={`${styles.charIconDiv}`}>
+      <div className={`${styles.skillProfile}`}>
+        <img className={`${styles.skillImage}`} src={skillIcons.normal}></img>
+        <div className={`${styles.skillInfoDiv}`}>
+          <p className={`${styles.skillName}`}>Normal Attack</p>
+          <div className={`${styles.skillInfoDivInner}`}>
+            <div className={`${styles.skillIconDiv}`}>
               <img
                 className={`${styles.setIcon}`}
                 src={skillIcons.control}
               ></img>
-              <p className={`${styles.charOtherText}`}> {data.skill[0].type}</p>
+              <p className={`${styles.skillOtherText}`}>
+                {" "}
+                {data.skill[0].type}
+              </p>
             </div>
-            <div className={`${styles.charIconDiv}`}>
+            <div className={`${styles.skillIconDiv}`}>
               <img className={`${styles.setIcon}`} src={skillIcons.ammo}></img>
-              <p className={`${styles.charOtherText} `}>{data.skill[0].ammo}</p>
+              <p className={`${styles.skillOtherText} `}>
+                {data.skill[0].ammo}
+              </p>
             </div>
-            <div className={`${styles.charIconDiv}`}>
+            <div className={`${styles.skillIconDiv}`}>
               <img
                 className={`${styles.setIcon}`}
                 src={skillIcons.reload}
               ></img>
-              <p className={`${styles.charOtherText} `}>
+              <p className={`${styles.skillOtherText} `}>
                 {data.skill[0].reload}
               </p>
             </div>
