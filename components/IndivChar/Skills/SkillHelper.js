748d9bbce93051bd74f3f36fd0b9be4eb1f96c3f
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-04-10 00:02:09 -0400

    update:add comments

--- components/IndivChar/Skills/SkillHelper.js
+++ components/IndivChar/Skills/SkillHelper.js
@@ -3,6 +3,7 @@ import react, { Fragment } from "react";
 import styles from "../../../styles/skill.module.css";
 const SkillTextFormat = ({ firstLetter }) => {
   console.log(firstLetter);
+  // Check if the first letter of the string is ■ and apply needed css to <p>
   if (firstLetter[0] === "■") {
     return (
       <Fragment>
