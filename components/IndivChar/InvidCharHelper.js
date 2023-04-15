6d2aa794747f763849e2c19d7a5c9f263fb27957
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-04-14 20:01:35 -0400

    refactor: add code for info refactor

---
+++ components/IndivChar/InvidCharHelper.js
@@ -0,0 +1,41 @@
+import react, { Fragment } from "react";
+import styles from "../../styles/combatinfo.module.css";
+const InfoHelper = ({ data, section }) => {
+  let temp = [];
+  let temp2 = [];
+  if (section === "Combat") {
+    const { rarity, unitClass, weapon, element, position } = data;
+    temp = [rarity, unitClass, weapon, element, position];
+    temp2 = ["Rarity", "Class", "Weapon", "Element", "Burst Type"];
+  }
+  return (
+    <Fragment>
+      <div className={`${styles.combatWholeDiv}`}>
+        <h2 className={`${styles.combatTitle} ${styles.combatText}  white`}>
+          {section} Info
+        </h2>
+        <div className={`${styles.combatDiv}`}>
+          {desc.map((element, index) => {
+            return (
+              <div className={`${styles.combatRowDiv}`}>
+                <p
+                  className={`${styles.combatLeftText} ${styles.combatText} white`}
+                >
+                  {temp2[index]}
+                </p>
+                <p
+                  className={`${styles.combatRightText} ${styles.combatText}  white`}
+                >
+                  {element}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        =
+      </div>
+    </Fragment>
+  );
+};
+
+export default InfoHelper;
