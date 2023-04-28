ccc1d2e0922bfdd6d3a583c2874dc0b9035ea990
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-04-27 23:04:56 -0400

    update: add ssr window tracking

--- components/IndivChar/InvidCharHelper.js
+++ components/IndivChar/InvidCharHelper.js
@@ -1,7 +1,9 @@
 import { Fragment } from "react";
 import { infoChar, infoOther, infoCombat } from "../../chardata";
+// import useWindowDimensions from "./hooks/useWindowDimensions";
 import styles from "../../styles/combatinfo.module.css";
 import mobileStyles from "../../styles/profilemobile.module.css";
+
 const InfoHelper = ({ data, section }) => {
   // LeftData is the static string eg, class, weapon
   // Rightdata is the dynamic string eg for weapon smg or sg
@@ -64,5 +66,67 @@ const InfoHelper = ({ data, section }) => {
   );
 };
 
-export const InfoHelperMobile = ({ data, section }) => {};
+export const InfoHelperMobile = ({ data, section }) => {
+  // LeftData is the static string eg, class, weapon
+  // Rightdata is the dynamic string eg for weapon smg or sg
+  let leftData,
+    rightData = [];
+  // Set data according to which section of charinfo we are on
+  if (section === "Info") {
+    const { name, manufacturer, squad } = data;
+    leftData = infoChar;
+    rightData = [name, manufacturer, squad];
+  } else if (section === "Combat") {
+    const { rarity, unitClass, weapon, element, position } = data;
+    leftData = infoCombat;
+    rightData = [rarity, unitClass, weapon, element, position];
+  } else if (section === "Other") {
+    const { role } = data;
+    leftData = infoOther;
+    rightData = [role];
+  }
+  return (
+    <Fragment>
+      <div className={`${mobileStyles.combatWholeDiv}`}>
+        <h2
+          className={`${mobileStyles.combatTitle} ${mobileStyles.combatText}  white`}
+        >
+          {section} Info
+        </h2>
+        {/* Render depending on if data is array or string */}
+        {section === "Other" ? (
+          <div className={`${mobileStyles.otherRoles}`}>
+            {rightData.map((eachRole, index) => (
+              <p
+                key={index}
+                className={`${mobileStyles.otherRoleText} ${mobileStyles.otherText} white`}
+              >
+                {eachRole}
+              </p>
+            ))}
+          </div>
+        ) : (
+          <div className={`${mobileStyles.combatDiv}`}>
+            {rightData.map((element, index) => {
+              return (
+                <div className={`${mobileStyles.combatRowDiv}`}>
+                  <p
+                    className={`${mobileStyles.combatLeftText} ${mobileStyles.combatText} white`}
+                  >
+                    {leftData[index]}
+                  </p>
+                  <p
+                    className={`${mobileStyles.combatRightText} ${mobileStyles.combatText}  white`}
+                  >
+                    {element}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </Fragment>
+  );
+};
 export default InfoHelper;
