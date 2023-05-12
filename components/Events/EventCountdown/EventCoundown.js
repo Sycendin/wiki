434c6779461755960c4573e7faee8c26d15e6046
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-05-11 22:42:54 -0400

    update: switch branch add intial files

---
+++ components/Events/EventCountdown/EventCoundown.js
@@ -0,0 +1,6 @@
+import react, { Fragment } from "react";
+import styles from "../../../styles/eventcountdown.module.css";
+const EventCountDown = () => {
+  return <Fragment></Fragment>;
+};
+export default EventCountDown;
