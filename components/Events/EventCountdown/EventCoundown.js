6ce15ebecaf085f874de871bdc3c292cb076497a
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-05-12 21:37:31 -0400

    update: test react-countdown package

--- components/Events/EventCountdown/EventCoundown.js
+++ components/Events/EventCountdown/EventCoundown.js
@@ -1,6 +1,14 @@
 import react, { Fragment } from "react";
+import Countdown from "react-countdown";
 import styles from "../../../styles/eventcountdown.module.css";
 const EventCountDown = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <Countdown
+        date={Date.now() + 10000}
+        className={`${styles.eventDiv} white`}
+      />
+    </Fragment>
+  );
 };
 export default EventCountDown;
--- components/Events/EventMain.js
+++ components/Events/EventMain.js
@@ -1,5 +1,13 @@
 import react, { Fragment } from "react";
+import EventCountDown from "./EventCountdown/EventCoundown";
+import styles from "../../styles/event.module.css";
 const EventMain = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className={`${styles.eventDiv}`}>
+        <EventCountDown />
+      </div>
+    </Fragment>
+  );
 };
 export default EventMain;
