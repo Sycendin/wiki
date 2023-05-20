e268184e964aea0a6807d8f24f79a02f4022510a
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-05-20 00:01:39 -0400

    update: add more files for past events

--- components/Events/EventMain.js
+++ components/Events/EventMain.js
@@ -1,11 +1,13 @@
 import react, { Fragment } from "react";
 import EventCountDown from "./EventCountdown/EventCoundown";
+import PastEvents from "./PastEvents/PastEvents";
 import styles from "../../styles/event.module.css";
 const EventMain = () => {
   return (
     <Fragment>
       <div className={`${styles.eventMainDiv}`}>
         <EventCountDown />
+        <PastEvents />
       </div>
     </Fragment>
   );
--- components/Events/PastEvents/PastEvents.js
+++ components/Events/PastEvents/PastEvents.js
@@ -1,8 +1,12 @@
 import react, { Fragment } from "react";
+import PastEventsList from "./PastEventsList/PastEventsList";
 const PastEvents = () => {
   return (
     <Fragment>
-      <div className={`${styles.pastEventsDiv}`}></div>
+      <div className={`${styles.pastEventsDiv}`}>
+        <PastEventsList />
+      </div>
     </Fragment>
   );
 };
+export default PastEvents;
