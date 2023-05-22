41e1bda6f61f6f2318c9c5030881ce1f4974429b
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-05-21 23:10:14 -0400

    update: add pastevent code

--- components/Events/PastEvents/PastEventsList/PastEventsList.js
+++ components/Events/PastEvents/PastEventsList/PastEventsList.js
@@ -1,10 +1,9 @@
 import react, { Fragment } from "react";
+
 const PastEventsList = () => {
   return (
     <Fragment>
-      <div className={`${styles.pastEventsListDiv}`}>
-        <div></div>
-      </div>
+      <div className={`${styles.pastEventsListDiv}`}></div>
     </Fragment>
   );
 };
