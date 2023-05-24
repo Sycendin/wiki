cf9662dc89c20452d1ba0554d4ccb1ff855c6359
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-05-24 02:15:35 -0400

    update: add more event data

--- eventdata.js
+++ eventdata.js
@@ -5,4 +5,64 @@ export const pastEvents = [
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/image-112.png?resize=1024%2C576&ssl=1",
   },
+  {
+    name: "License To Kill",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/FtkdX9iXoAImctp.jpg?resize=1024%2C576&ssl=1",
+  },
+  {
+    name: "Cherry Blossom",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/Update-on-March-30.jpg?resize=1024%2C576&ssl=1",
+  },
+  {
+    name: "Bow-wow Paradise",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/2023-03-13-16_44_36-.png?resize=1024%2C577&ssl=1",
+  },
+  {
+    name: "BULLET X CHAINSAW",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/event_banner_ce_01_l.png?w=1110&ssl=1",
+  },
+  {
+    name: "Maid in Valentine",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/Fn3c71VXEAEWRVG.jpg?resize=1024%2C576&ssl=1",
+  },
+  {
+    name: "D-Outsiders",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/D-Outsiders.png?w=1110&ssl=1",
+  },
+  {
+    name: "Brand New Year",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/Brand-New-Year.jpg?w=720&ssl=1",
+  },
+  {
+    name: "Miracle Snow",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/BCEF0A54-DCC3-4A11-B6BB-B77598ECA71C.png?w=1110&ssl=1",
+  },
+  {
+    name: "Hightech Toy",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/image-88.png?w=710&ssl=1",
+  },
+  {
+    name: "No Caller ID",
+    enddate: "May 20th 2023",
+    image:
+      "https://i0.wp.com/nikke.gg/wp-content/uploads/maxresdefault.jpg?resize=1024%2C576&ssl=1",
+  },
 ];
