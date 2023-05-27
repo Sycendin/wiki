215ee25ce22ac5e867c8e443becdb2aca983460a
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-05-26 23:40:47 -0400

    update: update eventdata

--- eventdata.js
+++ eventdata.js
@@ -11,66 +11,83 @@ export const pastEvents = [
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/image-112.png?resize=1024%2C576&ssl=1",
-    logo: " https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/e/e9/Bunny_X_777_logo.png",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/d/dc/Over_Zone_logo.png",
   },
   {
     name: "License To Kill",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/FtkdX9iXoAImctp.jpg?resize=1024%2C576&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/a/ab/License_to_Kill_logo.png",
+  },
+  {
+    name: "Full Fool day",
+    enddate: "May 20th 2023",
+    image:
+      "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/2/21/Event_Banner_Full_Fool_Day.png",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/6/6a/Full_Fool_Day_logo.png",
   },
   {
     name: "Cherry Blossom",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/Update-on-March-30.jpg?resize=1024%2C576&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/e/e4/Cherry_Blossom_logo.png",
   },
   {
     name: "Bow-wow Paradise",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/2023-03-13-16_44_36-.png?resize=1024%2C577&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/6/64/Bow-Wow_Paradise_logo.png",
   },
   {
     name: "BULLET X CHAINSAW",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/event_banner_ce_01_l.png?w=1110&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/c/c6/BULLET_X_CHAINSAW_logo.png",
   },
   {
     name: "Maid in Valentine",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/Fn3c71VXEAEWRVG.jpg?resize=1024%2C576&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/2/2f/Maid_in_Valentine_logo.png",
   },
   {
     name: "D-Outsiders",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/D-Outsiders.png?w=1110&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/f/f3/D-Outsiders_logo.png",
   },
   {
     name: "Brand New Year",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/Brand-New-Year.jpg?w=720&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/4/47/Brand_New_Year_logo.png",
   },
   {
     name: "Miracle Snow",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/BCEF0A54-DCC3-4A11-B6BB-B77598ECA71C.png?w=1110&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/2/2b/Miracle_Snow_logo.png",
   },
   {
     name: "Hightech Toy",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/image-88.png?w=710&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/f/f9/Hightech_Toy_logo.png",
   },
   {
     name: "No Caller ID",
     enddate: "May 20th 2023",
     image:
       "https://i0.wp.com/nikke.gg/wp-content/uploads/maxresdefault.jpg?resize=1024%2C576&ssl=1",
+    logo: "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/d/d7/No_Caller_ID_logo.png",
   },
 ];
