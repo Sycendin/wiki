264f6dd5aaa6d6360aa557cd928c24cbc07d56d7
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-06-07 00:00:35 -0400

    update: add pickup to events

--- components/Events/EventMain.js
+++ components/Events/EventMain.js
@@ -1,6 +1,7 @@
 import react, { Fragment } from "react";
 import EventCountDown from "./EventCountdown/EventCoundown";
 import PastEvents from "./PastEvents/PastEvents";
+import Pickup from "./Pickup/Pickup";
 import styles from "../../styles/event.module.css";
 const EventMain = () => {
   return (
@@ -8,6 +9,7 @@ const EventMain = () => {
       <div className={`${styles.eventMainDiv}`}>
         <EventCountDown />
         <PastEvents />
+        <Pickup />
       </div>
     </Fragment>
   );
