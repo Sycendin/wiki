e13be6bd8ce440ee594ad8ed0b0d1f6d34f0657c
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-06-18 23:16:09 -0400

    update: refactor homebutton

--- components/Button/HomeButton.js
+++ components/Button/HomeButton.js
@@ -1,10 +1,10 @@
 import react, { Fragment } from "react";
 import Link from "next/link";
 import styles from "../../styles/button.module.scss";
-const HomeButton = () => {
+const HomeButton = (link) => {
   return (
     <Fragment>
-      <Link href="/">
+      <Link href={link}>
         <button className="button is-primary">Go home</button>
       </Link>
     </Fragment>
