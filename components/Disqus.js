136fbaea0ef1382fab92fa85400807b737cde675
Brandon <37314885+Sycendin@users.noreply.github.com>
2023-06-30 03:35:37 -0400

    update: make disqus comp more modualr

--- components/Disqus.js
+++ components/Disqus.js
@@ -4,8 +4,8 @@ const Disqus = ({ url, identifier, title }) => {
   const disqusShortname = "nikkegame";
   const disqusConfig = {
     url: "http://localhost:3000",
-    identifier: "pickup",
-    title: "pickup",
+    identifier: identifier,
+    title: title,
   };
   return (
     <DiscussionEmbed
